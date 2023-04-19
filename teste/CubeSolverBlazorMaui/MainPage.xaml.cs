0d0e8842dacf2e2ed1d8db6ad8334657647b7189
unknown <emanuellybs14@gmail.com>
2023-04-19 14:07:19 -0300

    teste

---
+++ teste/CubeSolverBlazorMaui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CubeSolverBlazorMaui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ teste/CubeSolverBlazorMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CubeSolverBlazorMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
