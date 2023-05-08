2fa1330cd171c56a09a015d85100a0bda5a21dd5
andrevpc <andrevictor06@gmail.com>
2023-05-08 16:53:08 -0300

    Integração feita

--- CubeSolverBlazorMaui/CubeSolver/Program.cs
+++ CubeSolverBlazorMaui/CubeSolver/Program.cs
@@ -1,51 +1,53 @@
-﻿using System;
+﻿//using CubeSolverBlazorMaui.CubeSolver;
 
-ConsoleKeyInfo cki;
-Cubo cubinho = new Cubo();
+//using System;
 
-byte[] validos = {87, 89, 71, 66, 79, 82};
+//ConsoleKeyInfo cki;
+//CubeSolver.Cubo cubinho = new Cubo();
 
-// for (int i = 0; i < 6; i++)
-// {
+//byte[] validos = {87, 89, 71, 66, 79, 82};
 
-//     Console.WriteLine("-----------------");
-//     Console.WriteLine($"FACE {i+1}");
-//     Console.WriteLine("-----------------");
+//// for (int i = 0; i < 6; i++)
+//// {
 
-//     byte[] cores = new byte[9];
-//     for (int j = 0; j < 9; j++)
-//     {
-//         while(true)
-//         {
-//             Console.WriteLine($"\nInsira o valor {j+1}");
-//             cki = Console.ReadKey();
-//             if(Array.IndexOf(validos, Convert.ToByte(cki.Key)) > -1)
-//             {
-//                 cores[j] = Convert.ToByte(cki.Key);
-//                 break;
-//             }
-//             Console.WriteLine("Valor inválido");
-//         }
-//     }
-//     Console.WriteLine("\n");
+////     Console.WriteLine("-----------------");
+////     Console.WriteLine($"FACE {i+1}");
+////     Console.WriteLine("-----------------");
 
-//     Face facezinha = new Face(cores);
-//     cubinho.addFace(facezinha, i);
+////     byte[] cores = new byte[9];
+////     for (int j = 0; j < 9; j++)
+////     {
+////         while(true)
+////         {
+////             Console.WriteLine($"\nInsira o valor {j+1}");
+////             cki = Console.ReadKey();
+////             if(Array.IndexOf(validos, Convert.ToByte(cki.Key)) > -1)
+////             {
+////                 cores[j] = Convert.ToByte(cki.Key);
+////                 break;
+////             }
+////             Console.WriteLine("Valor inválido");
+////         }
+////     }
+////     Console.WriteLine("\n");
 
-//     Console.Write("\n");
-//     foreach (var item in cores)
-//     {
-//         Console.Write(item);
-//     }
-//     Console.Write("\n");
-// }
+////     Face facezinha = new Face(cores);
+////     cubinho.addFace(facezinha, i);
 
-cubinho.translateAlg("F2 R2 F B2 R B D2 F' B' R' L' B L B' R D L' U D2 F' B' L2 D' R' D");
-Solver solucao = new Solver(cubinho);
-Console.WriteLine(solucao.Cross());
-Console.WriteLine(solucao.FirstCorners());
-Console.WriteLine(solucao.CenterEdges());
-Console.WriteLine(solucao.TopCross());
-Console.WriteLine(solucao.TopCorners());
-Console.WriteLine(solucao.PLL1());
-Console.WriteLine(solucao.Minerva());
+////     Console.Write("\n");
+////     foreach (var item in cores)
+////     {
+////         Console.Write(item);
+////     }
+////     Console.Write("\n");
+//// }
+
+//cubinho.translateAlg("F2 R2 F B2 R B D2 F' B' R' L' B L B' R D L' U D2 F' B' L2 D' R' D");
+//Solver solucao = new Solver(cubinho);
+//Console.WriteLine(solucao.Cross());
+//Console.WriteLine(solucao.FirstCorners());
+//Console.WriteLine(solucao.CenterEdges());
+//Console.WriteLine(solucao.TopCross());
+//Console.WriteLine(solucao.TopCorners());
+//Console.WriteLine(solucao.PLL1());
+//Console.WriteLine(solucao.Minerva());
